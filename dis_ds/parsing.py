0d908bc66079ef8ac17c22bdc437decbbaa8835d
Ian Huston <ihuston@pivotal.io>
2015-09-02 14:26:52 +0100

    Implemented function to get status severities from a file.
    Signed-off-by: Ellie Casey <elcasey@pivotal.io>

--- dis_ds/parsing.py
+++ dis_ds/parsing.py
@@ -1,6 +1,15 @@
 # Parsing module for dis
 #
+import pandas as pd
 
 
 def get_status_severities(file):
-    return {}
+    if file == "":
+        return {}
+
+    df = pd.read_json(file)
+    severities = {}
+    for line in df['lineStatuses']:
+        for status in line:
+            severities[status['statusSeverity']] = status['statusSeverityDescription']
+    return severities
--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -12,3 +12,33 @@ class TestStatusSeverities(TestCase):
         self.assertEqual(result, {})
         return
 
+    def test_single_severity(self):
+        single_severity_file = '[{"lineStatuses":[{"statusSeverity":6, "statusSeverityDescription":"Severe Delays"}]}]'
+
+        result = parsing.get_status_severities(single_severity_file)
+        expected = {6: "Severe Delays"}
+        self.assertEqual(expected, result)
+        return
+
+    def test_multiple_severity(self):
+        multiple_severity_file = """
+        [{"lineStatuses":[{"statusSeverity":6, "statusSeverityDescription":"Severe Delays"}]},
+         {"lineStatuses":[{"statusSeverity":10, "statusSeverityDescription":"Good Service"}]}]
+        """
+
+        result = parsing.get_status_severities(multiple_severity_file)
+        expected = {6: 'Severe Delays', 10: 'Good Service'}
+        self.assertEqual(expected, result)
+        return
+
+    def test_multiple_severities_for_a_single_line(self):
+        multiple_severity_file = """
+        [{"lineStatuses":[{"statusSeverity":6, "statusSeverityDescription":"Severe Delays"},
+                          {"statusSeverity":9, "statusSeverityDescription":"Minor Delays"}]}]
+        """
+
+        result = parsing.get_status_severities(multiple_severity_file)
+        expected = {6: 'Severe Delays', 9: 'Minor Delays'}
+        self.assertEqual(expected, result)
+        return
+
